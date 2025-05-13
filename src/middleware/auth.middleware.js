68132ced76af2c3041b2a9a2b14551f7e6327ca2
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2025-05-13 09:56:21 +0300

    Authentication: protect middleware:  fixing handling expired tokens, invalid tokens or other errors in verifyToken. Catching other errors in general.

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,36 +1,68 @@
 // middleware/auth.middleware.js
-const jwt = require('../utils/jwt');
-const db = require('../config/db');
+const jwt = require("../utils/jwt");
+const db = require("../config/db");
 
 const protect = async (req, res, next) => {
   try {
     let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1];
+    if ( req.headers.authorization && req.headers.authorization.startsWith("Bearer")) 
+      {
+      // Extract token from Bearer token
+      token = req.headers.authorization.split(" ")[1];
     }
 
     if (!token) {
       return res.status(401).json({
-        status: 'fail',
-        message: 'You are not logged in',
+        success: false,
+        message: "You are not logged in",
       });
     }
 
-    const decoded = jwt.verifyToken(token);
-    const result = await db.query('SELECT * FROM owners WHERE id = $1', [decoded.id]);
+    let decoded;
+
+    try {
+      decoded = jwt.verifyToken(token);
+    } catch (err) {
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({
+          success: false,
+          message: "Your session has expired. Please log in again",
+        });
+      }
+      if (err.name === "JsonWebTokenError") {
+        return res.status(401).json({
+          success: false,
+          message: "Invalid token. Please log in again", //invalid signatur, content or format
+        });
+      }
+
+      return res.status(500).json({
+        success: false,
+        error: "Internal server error during authentiction.",
+        message: err.message,
+      });
+    }
+
+    const result = await db.query("SELECT * FROM owners WHERE id = $1", [
+      decoded.id,
+    ]);
+
     const currentOwner = result.rows[0];
 
     if (!currentOwner) {
       return res.status(401).json({
-        status: 'fail',
-        message: 'The owner belonging to this token no longer exists',
+        success: false,
+        message: "The owner belonging to this token no longer exists",
       });
     }
 
     req.owner = currentOwner;
     next();
   } catch (err) {
-    next(err);
+    return res.status(500).json({
+      success: false,
+      message: "An authentication error occurred.",
+    });
   }
 };
 
