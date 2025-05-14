4c5f3a2ec7baa14e8aa2b257e3bced34795c6393
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2025-05-14 10:18:24 +0300

    Utils: fileupload:  middleware for receiving files in http requests as form-data.

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,42 @@
+const multer = require('multer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const ownerId = req.owner.id;
+    const itemName = req.body.name || 'temp';
+    const uploadPath = path.join(__dirname, `../../images/${ownerId}/${req.body.category_id}/${itemName.replace(/[^a-z0-9]/gi, '_')}`);
+    
+    fs.mkdirSync(uploadPath, { recursive: true });
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+    const filename = `${uuidv4()}${ext}`;
+    cb(null, filename);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const validExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (validExtensions.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only image files are allowed!'), false);
+  }
+};
+
+
+
+const upload = multer({ 
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+
+module.exports = {
+    upload,
+};
